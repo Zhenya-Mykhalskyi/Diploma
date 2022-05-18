de483b451ce35160424e0d69246f6dcd4ea7766d
Zhenya Mykhaslskyi <zmykhalskyi@gmail.com>
2022-05-18 16:45:47 +0300

    add applicant page

--- src/components/Applicant/Applicant.jsx
+++ src/components/Applicant/Applicant.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import FormSection from "../Main/FormSection/FormSection";
+import QuestionsBar from "../Main/QuestionsBar/QuestionsBar";
 import './Applicant.scss';
+import Reviews from "./Reviews/Reviews";
+import Skills from "./Skills/Skills";
+import Timetables from "./Timetables/Timetables";
 
 
 
 const Applicant =(props) => {
     return (
-       <div>Applicant page</div>
+       <div>
+           <Timetables />
+           <Reviews />
+           <Skills />
+           <QuestionsBar />
+           <FormSection />
+       </div>
     )
 }
 
---
+++ src/components/Applicant/Timetables/Timetables.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import'./Timetables.scss';
+
+const Timetables = (props) => {
+    return (
+        <div class="timetables">
+            <div class="timetables-refs">
+                <a href="http://www.tk.te.ua/index.php/2014-03-15-19-58-16/2014-08-31-12-06-17" target="_blank" class="refs-item">Ознайомитись із правилами вступу</a>
+                <a href="http://www.tk.te.ua/index.php/2014-03-15-19-58-16/2014-08-31-12-06-58" target="_blank" class="refs-item">Документи, які потрібні для вступу</a>
+                <a href="https://jobs.dou.ua/salaries/" target="_blank" class="refs-item">Ознайомитись із зарплатами в ІТ-сфері </a>
+                <a href="https://nv.ua/ukr/techno/it-industry/vvijti-v-it-top-10-najpotribnishih-it-spetsialnostej-v-ukrajini-2242280.html" target="_blank"  class="refs-item">Топ-10  IT-спеціальностей в Україні  </a>
+            </div>
+    </div>
+    )
+}
+
+export default Timetables;
